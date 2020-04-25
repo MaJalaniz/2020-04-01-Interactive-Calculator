236c64a66da03a8a2e990bf93fca15604d0ee91f
Matthew Alaniz <alaniz.matthew07@gmail.com>
2020-04-25 18:48:55 -0500

    writing out an additional feature

--- js/calc.js
+++ js/calc.js
@@ -1,9 +1,15 @@
 
-
 function total(inputBill, inputTip){
      inputBill = document.getElementById("bill").value;
      inputTip = document.getElementById("tip").value;
      let total = inputBill * inputTip;
      let totalBill = document.getElementById("displayTotal").innerHTML = "$" + total;
     return  totalBill;
+}
+function dividingTotal(inputBill, groupTotal){
+    inputBill = document.getElementById("bill").value;
+    groupTotal = document.getElementById("groupAmount").value;
+    let divideMeal = inputBill / groupTotal;
+    let totalAmount = document.getElementById("displayTotal").innerHTML = "$" + divideMeal;
+    return divideMeal;
 }
