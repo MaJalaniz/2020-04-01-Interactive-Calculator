c94c96ebf0ec2060bc8d2bda6d12b782ae36f3a2
Matthew Alaniz <alaniz.matthew07@gmail.com>
2020-04-25 20:30:03 -0500

    no letters for both functions

--- js/calc.js
+++ js/calc.js
@@ -4,20 +4,24 @@ function total(inputBill, inputTip){
      inputTip = document.getElementById("tip").value;
      
      let total = parseFloat((inputBill * (inputTip / 100))).toFixed(2);
-     let totalBill = document.getElementById("displayTotal").innerHTML = "$" + total;
+     let letters = /^[a-zA-Z]+$/;
 
-    return  totalBill;
+     if(inputBill.match(letters) || inputTip.match(letters)){
+        total = document.getElementById("displayTotal").innerHTML = "$" + 0;
+     }else{
+       total = document.getElementById("displayTotal").innerHTML = "$" + total;
+     }
+    return  total;
 }
 
 function dividingTotal(inputBill, groupTotal){
-    
     inputBill = document.getElementById("bill").value;
     groupTotal = document.getElementById("groupAmount").value;
-    
+
     let letters = /^[a-zA-Z]+$/;
     let divideMeal = parseFloat(inputBill / groupTotal).toFixed(2);
    
-    if(inputBill.match(letters)){
+    if(inputBill.match(letters) || groupTotal.match(letters)){
          totalAmount = document.getElementById("displayTotal").innerHTML = "$" + 0;
     }else{
         totalAmount = document.getElementById("displayTotal").innerHTML = "$" + divideMeal;
