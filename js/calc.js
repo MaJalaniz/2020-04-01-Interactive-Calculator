cee6a872d4d2f7a2affac12a0f606e1f73b39cc1
Matthew Alaniz <alaniz.matthew07@gmail.com>
2020-04-25 19:48:18 -0500

    converting number to decimal for user input

--- js/calc.js
+++ js/calc.js
@@ -2,14 +2,19 @@
 function total(inputBill, inputTip){
      inputBill = document.getElementById("bill").value;
      inputTip = document.getElementById("tip").value;
-     let total = inputBill * inputTip;
+     
+     let total = parseFloat((inputBill * (inputTip / 100))).toFixed(2);
      let totalBill = document.getElementById("displayTotal").innerHTML = "$" + total;
+
     return  totalBill;
 }
+
 function dividingTotal(inputBill, groupTotal){
     inputBill = document.getElementById("bill").value;
     groupTotal = document.getElementById("groupAmount").value;
+
     let divideMeal = inputBill / groupTotal;
     let totalAmount = document.getElementById("displayTotal").innerHTML = "$" + divideMeal;
+
     return divideMeal;
 }
